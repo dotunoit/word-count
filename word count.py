0afc59317d46ec73ef46c4a48f32fe0fddd32924
DOTUN ALEX OMOBOYE <133263016+dotunoit@users.noreply.github.com>
2023-05-22 15:55:55 +0100

    Update word count.py

--- word count.py
+++ word count.py
@@ -1,58 +1,17 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "ef5ed251",
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#The 2 variables to sort the sentence into digit & letters \n",
-    "digits = 0\n",
-    "letters = 0\n",
-    "\n",
-    "#Accepting Sentence to be analysed\n",
-    "sentence = input(\"Please enter a sentence: \")\n",
-    "\n",
-    "# For loop to count through the sentence\n",
-    "for char in sentence:\n",
-    "    if char.isalpha():\n",
-    "        letters += 1\n",
-    "    if char.isdigit():\n",
-    "        digits+= 1\n",
-    "#Outputs \n",
-    "print(\"LETTERS:\", letters)\n",
-    "print(\"DIGITS:\", digits) "
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "id": "729ce1b0",
-   "metadata": {},
-   "outputs": [],
-   "source": []
-  }
- ],
- "metadata": {
-  "kernelspec": {
-   "display_name": "Python 3 (ipykernel)",
-   "language": "python",
-   "name": "python3"
-  },
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": "3.10.9"
-  }
- },
- "nbformat": 4,
- "nbformat_minor": 5
-}
+#Accepting Sentence to be analysed 
+sentence = input("Enter the sentence: ")
+
+# Initializing variables to count letters and digits
+letter = 0
+digit = 0
+
+# For loop to count through the sentence
+for char in sentence:
+    if char.isalpha():
+        letter += 1
+    elif char.isdigit():
+        digit += 1
+
+# Print Output
+print("LETTERS: ", letter)
+print("DIGITS: ", digit) 
